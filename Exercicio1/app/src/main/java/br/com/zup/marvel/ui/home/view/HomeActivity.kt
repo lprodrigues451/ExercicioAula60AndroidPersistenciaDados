984bf2d15f48fae34b66de1898439cc70bb13e87
lprodrigues451 <lprodrigues451@gmail.com>
2022-07-06 21:49:09 -0300

    Subindo que editei na cor do botão na Activity home

--- Exercicio1/app/src/main/java/br/com/zup/marvel/ui/home/view/HomeActivity.kt
+++ Exercicio1/app/src/main/java/br/com/zup/marvel/ui/home/view/HomeActivity.kt
@@ -5,21 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
-import br.com.zup.marvel.databinding.ActivityMainBinding
+import br.com.zup.marvel.databinding.ActivityHomeBinding
 import br.com.zup.marvel.domain.model.Marvel
 import br.com.zup.marvel.ui.home.viewmodel.HomeViewModel
 import br.com.zup.marvel.ui.marvelliist.view.MarvelListaActivity
 import br.com.zup.marvel.ui.viewstate.ViewState
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityHomeBinding
 
     private val viewModel: HomeViewModel by lazy {
         ViewModelProvider(this)[HomeViewModel::class.java]
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initObserver()
         clickOnButtonAdd()
